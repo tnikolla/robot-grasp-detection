e49fbd1443ad52a8610ff9663d052ccd1dd0b699
Tomi Nikolla <tomi.nikolla@gmail.com>
2017-06-01 11:40:00 +0000

    a mature version

--- grasp_detection.py
+++ grasp_detection.py
@@ -10,105 +10,105 @@ import numpy as np
 import inference
 import tensorflow as tf
 FLAGS = None
-#TRAIN_FILE = glob.glob("/root/imagenet-data/train*")
-TRAIN_FILE = 'train-00000-of-01024'
-#glob.glob("/home/iki/master_project/cgd/0?/pcd*r.png")
+TRAIN_FILE = '/root/imagenet-data/train-00000-of-01024'
 VALIDATION_FILE = 'validation.tfrecords'
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
 
-'''
-label='n03'
-labels = np.array(['n02', 'n03', 'n04'])
-y = [label == i for i in labels]
-y=np.array(y)
-y=y.astype(int)
-print(y)
+def data_files():
+    tf_record_pattern = os.path.join(FLAGS.data_dir, '%s-*' % 'train')
+    data_files = tf.gfile.Glob(tf_record_pattern)
+    return data_files
 
-labels = ['n02666624', 'n02860415', 'n02880940', 
-          'n02883344', 'n02908217', 'n02960690',
-          'n03003091', 'n03147509', 'n03261776',
-          'n03294833', 'n03438863', 'n03665924',
-          'n03690938', 'n03793489', 'n03797390',
-          'n03805725', 'n03848348', 'n03874599',
-          'n03904909', 'n04148054', 'n04154938',
-          'n04284002', 'n04303497', 'n04356056',
-          'n04450749', 'n07607605']
-'''
 def read_and_decode(filename_queue):
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)
-    features = tf.parse_single_example(
-        serialized_example,
-        features={
-            'image/class/label': tf.FixedLenFeature([], tf.int64),
-            'image/encoded': tf.FixedLenFeature([], tf.string)
-        })
-    image = tf.decode_raw(features['image/encoded'], tf.uint8)
-    image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-    image = tf.image.resize_images(image, [224, 224, 3])
-    #image_shape = tf.stack([IMAGE_HEIGHT, IMAGE_WIDTH, 3])
-    #image = tf.reshape(image, image_shape)
+    feature_map = {
+        'image/encoded': tf.FixedLenFeature([], tf.string, default_value=''),
+        'image/class/label': tf.FixedLenFeature([1], tf.int64, default_value=-1),
+        'image/class/text': tf.FixedLenFeature([], tf.string, default_value='')
+    }
+    features = tf.parse_single_example( serialized_example, feature_map)
+    image = tf.image.decode_jpeg(features['image/encoded'], channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize_images(image, [224,224])
     label = tf.cast(features['image/class/label'], tf.int32)
     return image, label
 
 def inputs(train, batch_size, num_epochs):
     if not num_epochs: num_epochs = None
-    filename = os.path.join('/root/imagenet-data', TRAIN_FILE if train else VALIDATION_FILE)
-    #filename = TRAIN_FILE
+    
     with tf.name_scope('input'):
+        data_files_ = data_files()
         filename_queue = tf.train.string_input_producer(
-            [filename], num_epochs=num_epochs)
+            data_files_, num_epochs=num_epochs)
         image, label = read_and_decode(filename_queue)
         images, sparse_labels = tf.train.shuffle_batch(
-            [image, label], batch_size=batch_size, num_threads=2,
+            [image, label], batch_size=batch_size, num_threads=4,
             capacity=1000+3*batch_size,
             min_after_dequeue=1000)
-        return images, sparse_labels
+        return images, tf.reshape(sparse_labels, [batch_size])
 
-def training(loss, learning_rate):
-    #tf.summary.scalar('loss', loss)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.minimize(loss)
-    return train_op
+#def training(loss, learning_rate):
+#    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+#    train_op = optimizer.minimize(loss)
+#    return train_op
 
 def run_training():
     with tf.Graph().as_default():
         images, labels = inputs(train=True,
                                 batch_size=FLAGS.batch_size,
                                 num_epochs=FLAGS.num_epochs)
-        
-        labels = tf.one_hot(labels, 1000)
-        print("images: {}".format(images.get_shape()))
-        print("labels: {}".format(labels.get_shape()[-1].value))        
+        labels_one = tf.one_hot(labels, 1000)
+        #labels_one = tf.reshape(labels_one, [32,1000])
+        print('labels: %s'%(labels.get_shape()))
+        print('labels_one: %s'%(labels_one.get_shape()))
+        print('labels dims: %s'%(labels.get_shape().ndims))
         logits = inference.inference(images)
+        print('logits: %s'%(logits.get_shape()))
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=logits, labels=labels))
-        train_op = training(loss, FLAGS.learning_rate)
-        init_op = tf.group(tf.global_variables_initializer(),
-			tf.local_variables_initializer())
-
+            logits=logits, labels=labels_one))
+        
+        tf.summary.scalar('loss', loss)
+        #la_argmax = tf.argmax(labels,2)
+        #lo_argmax = tf.argmax(logits,1)
+        
+        #a=logits
+        #b=tf.reshape(tf.cast(labels, tf.int32), [-1])
+        #labels_max = tf.reduce_max(b)
+        #logits_max = tf.reduce_max(a)
+        #top_k_op = tf.nn.in_top_k(a, b, 500)
+        correct_pred = tf.equal( tf.argmax(logits,1), tf.argmax(labels_one,1))
+        accuracy = tf.reduce_mean( tf.cast( correct_pred, tf.float32))
+        #correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
+        #accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
+        merged_summary_op = tf.summary.merge_all()
+        train_op = tf.train.GradientDescentOptimizer(learning_rate=FLAGS.learning_rate).minimize(loss)        
+        #train_op = training(loss, FLAGS.learning_rate)
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        #init_op = tf.initialize_all_variables()
         sess = tf.Session()
         sess.run(init_op)
+        summary_writer = tf.summary.FileWriter(FLAGS.log_dir)
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
+        
         try:
             step = 0
-            print('o')
+            start_time = time.time()
             while not coord.should_stop():
-                print('ok')
-                start_time = time.time()
-                print('oko')
-                _, loss_value = sess.run([train_op, loss])
-                print('okok')
-                duration = time.time() - start_time
-                print('okoko')
-                if step % 100 == 0:
-                    print('Step %d: loss = %.2f (%.3f sec)')%(step, loss_value, duration)
-            step +=1
+                start_batch = time.time()
+                _, loss_value, pred, acc, summary = sess.run(
+                    [train_op, loss, correct_pred, accuracy,  merged_summary_op])
+                summary_writer.add_summary(summary, step*FLAGS.batch_size)
+                duration = time.time() - start_batch
+                if step % 500 == 0:
+                    print('Step %d | loss = %.2f | accuracy = %.2f (%.3f sec/batch)')%(
+                        step, loss_value, acc, duration)
+                step +=1
         except tf.errors.OutOfRangeError:
-            print('Done training for %d epochs, %d steps.' % (FLAGS.num_epochs, step))
+            print('Done training for %d epochs, %d steps, %.1f min.' % (FLAGS.num_epochs, step, (time.time()-start_time)/60))
         finally:
             coord.request_stop()
 
@@ -123,11 +123,11 @@ if __name__ == '__main__':
     parser.add_argument(
         '--learning_rate',
         type=float,
-        default=0.01,
+        default=0.001,
         help='Initial learning rate.'
     )
     parser.add_argument(
-        '--train_dir',
+        '--data_dir',
         type=str,
         default='/root/imagenet-data/',
         help='Directory with training data.'
@@ -135,14 +135,20 @@ if __name__ == '__main__':
     parser.add_argument(
         '--num_epochs',
         type=int,
-        default=100,
+        default=10,
         help='Number of epochs to run trainer.'
     )
     parser.add_argument(
         '--batch_size',
         type=int,
-        default=100,
+        default=32,
         help='Batch size.'
+    )
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default='/tmp/tf/exp',
+        help='Tensorboard log_dir.'
     )
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- inference.py
+++ inference.py
@@ -21,44 +21,44 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 def inference(images):
-    print(images.get_shape())
+    #print(images.get_shape())
     # conv1 pool1 layer
     W_conv1 = weight_variable([5,5,3,64])
     b_conv1 = bias_variable([64])
     h_conv1 = tf.nn.relu(conv2d_s2(images, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
-    print(h_pool1.get_shape())
+    #print(h_pool1.get_shape())
     
     # conv2 pool layer
     W_conv2 = weight_variable([3,3,64,128])
     b_conv2 = bias_variable([128])
     h_conv2 = tf.nn.relu(conv2d_s2(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
-    print(h_pool2.get_shape())
+    #print(h_pool2.get_shape())
     
     # conv3 pool layer
     W_conv3 = weight_variable([3,3,128,256])
     b_conv3 = bias_variable([256])
     h_conv3 = tf.nn.relu(conv2d_s1(h_pool2, W_conv3) + b_conv3)
     h_pool3 = max_pool_2x2(h_conv3)
-    print(h_pool3.get_shape())
+    #print(h_pool3.get_shape())
     # fc1 layer
     W_fc1 = weight_variable([7*7*256, 512])
     b_fc1 = bias_variable([512])
     h_pool3_flat = tf.reshape(h_pool3, [-1, 7*7*256])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
-    print("h_fc1: {}".format(h_fc1.get_shape()))
+    #print("h_fc1: {}".format(h_fc1.get_shape()))
 
     # fc1 layer
     W_fc2 = weight_variable([512, 512])
     b_fc2 = bias_variable([512])
     h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
-    print("h_fc2: {}".format(h_fc2.get_shape()))
+    #print("h_fc2: {}".format(h_fc2.get_shape()))
 
     # fc1 layer
     W_fc3 = weight_variable([512, 1000])
     b_fc3 = bias_variable([1000])
     output = tf.nn.relu(tf.matmul(h_fc2, W_fc3) + b_fc3)
-    print("output: {}".format(output.get_shape()))
+    #print("output: {}".format(output.get_shape()))
     
     return output
