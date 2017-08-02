cd2e203023f3a78fe75ed44a80747cf0c0e1a2e8
Tomi Nikolla <tomi.nikolla@gmail.com>
2017-08-02 11:52:57 +0200

    adding image_processing.py

--- grasp_detection.py
+++ grasp_detection.py
@@ -40,71 +40,75 @@ def inputs(train, batch_size, num_epochs):
     if not num_epochs: num_epochs = None
     data_files_ = data_files()
     filename_queue = tf.train.string_input_producer(
-        [VALIDATION_FILE], num_epochs=num_epochs)
+        data_files_, num_epochs=num_epochs)
     image, label = read_and_decode(filename_queue)
     images, sparse_labels = tf.train.shuffle_batch(
         [image, label], batch_size=batch_size, num_threads=4,
         capacity=1000+3*batch_size,
         min_after_dequeue=1000)
+    # -- 1 --
+    # images = image_processing.distorted_inputs(images)
+    images = image_processing.distorted_inputs(images)
     return images, tf.reshape(sparse_labels, [batch_size])
 
 def run_training():
-    with tf.Graph().as_default():
-        images, labels = inputs(train=True,
-                                batch_size=FLAGS.batch_size,
-                                num_epochs=FLAGS.num_epochs)
-        labels_one = tf.one_hot(labels, 1000)
-        logits = inference_redmon.inference(images)
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=logits, labels=labels_one))        
-        tf.summary.scalar('loss', loss)
-        correct_pred = tf.equal( tf.argmax(logits,1), tf.argmax(labels_one,1))
-        accuracy = tf.reduce_mean( tf.cast( correct_pred, tf.float32))
-        tf.summary.scalar('accuracy', accuracy)
-        merged_summary_op = tf.summary.merge_all()
-        global_step=tf.Variable(0,trainable=False)
-        lr=tf.train.exponential_decay(FLAGS.learning_rate, 
-                                      global_step=global_step, 
-                                      decay_steps=800000,
-                                      decay_rate=0.16,
-                                      staircase=True)
-        train_op = tf.train.GradientDescentOptimizer(lr).minimize(loss,global_step=global_step)
-        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-        saver = tf.train.Saver()
-        sess = tf.Session()
-        sess.run(init_op)
-        summary_writer = tf.summary.FileWriter(FLAGS.log_dir)
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-        #restore model        
-        #saver.restore(sess, FLAGS.model_path)        
-        try:
-            step = 0
-            start_time = time.time()
-            while not coord.should_stop():
-                start_batch = time.time()
-                #train                
-                _, loss_value, pred, acc, summary = sess.run(
-                    [train_op, loss, correct_pred, accuracy,  merged_summary_op])
-                #evaluate
-                #loss_value, pred, acc, summary = sess.run(
-                #    [loss, correct_pred, accuracy,  merged_summary_op])
-                summary_writer.add_summary(summary, step*FLAGS.batch_size)
-                duration = time.time() - start_batch
-                if step % 10 == 0:
-                    print('Step %d | loss = %.2f | accuracy = %.2f (%.3f sec/batch)')%(
-                        step, loss_value, acc, duration)
-                step +=1
-        except tf.errors.OutOfRangeError:
-            print('Done training for %d epochs, %d steps, %.1f min.' % (FLAGS.num_epochs, step, (time.time()-start_time)/60))
-        finally:
-            coord.request_stop()
+    images, labels = inputs(train=True,
+                            batch_size=FLAGS.batch_size,
+                            num_epochs=FLAGS.num_epochs)
+    # -- 2 --        
+    #images = distorted_inputs(images)
+    labels_one = tf.one_hot(labels, 1000)
+    logits = inference_redmon.inference(images)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits, labels=labels_one))        
+    tf.summary.scalar('loss', loss)
+    correct_pred = tf.equal( tf.argmax(logits,1), tf.argmax(labels_one,1))
+    accuracy = tf.reduce_mean( tf.cast( correct_pred, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
+    merged_summary_op = tf.summary.merge_all()
+    global_step=tf.Variable(0,trainable=False)
+    lr=tf.train.exponential_decay(FLAGS.learning_rate, 
+                                  global_step=global_step, 
+                                  decay_steps=800000,
+                                  decay_rate=0.16,
+                                  staircase=True)
+    train_op = tf.train.GradientDescentOptimizer(lr).minimize(loss,global_step=global_step)
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    saver = tf.train.Saver()
+    sess = tf.Session()
+    sess.run(init_op)
+    summary_writer = tf.summary.FileWriter(FLAGS.log_dir)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    #restore model        
+    #saver.restore(sess, FLAGS.model_path)        
+    try:
+        step = 0
+        start_time = time.time()
+        while not coord.should_stop():
+            start_batch = time.time()
+            #train                
+            _, loss_value, pred, acc, summary = sess.run(
+            [train_op, loss, correct_pred, accuracy,  merged_summary_op])
+            #evaluate
+            #loss_value, pred, acc, summary = sess.run(
+            #    [loss, correct_pred, accuracy,  merged_summary_op])
+            summary_writer.add_summary(summary, step*FLAGS.batch_size)
+            duration = time.time() - start_batch
+            if step % 10 == 0:
+                print('Step %d | loss = %.2f | accuracy = %.2f (%.3f sec/batch)')%(
+                step, loss_value, acc, duration)
+            step +=1
+    except tf.errors.OutOfRangeError:
+        print('Done training for %d epochs, %d steps, %.1f min.' % (FLAGS.num_epochs, step, (time.time()-start_time)/60))
+    finally:
+        coord.request_stop()
 
-        saver.save(sess, FLAGS.model_path)
-        print('Model saved in: %s' % FLAGS.model_path)
-        #print('Evaluated from restored variables from: %s' % FLAGS.model_path)
-        coord.join(threads)
-        sess.close()
+    saver.save(sess, FLAGS.model_path)
+    print('Model saved in: %s' % FLAGS.model_path)
+    #print('Evaluated from restored variables from: %s' % FLAGS.model_path)
+    coord.join(threads)
+    sess.close()
 
 def main(_):
     run_training()
---
+++ image_processing.py
@@ -0,0 +1,117 @@
+import tensorflow as tf
+import numpy as np
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer('batch_size', 32,
+                            """Number of images to process in a batch.""")
+tf.app.flags.DEFINE_integer('image_size', 224,
+                            """Provide square images of this size.""")
+tf.app.flags.DEFINE_integer('num_preprocess_threads', 8,
+                            """Number of preprocessing threads per tower. """
+                            """Please make this a multiple of 4.""")
+
+def distorted_inputs(data_files, batch_size=None, num_preprocess_threads=None):
+    with tf.device('/cpu:0:'):
+        images, labels = batch_inputs(
+            data_files, batch_size, train=True,
+            num_preprocess_threads=num_preprocess_threads,
+            num_readers=FLAGS.num_readers)
+    return images, labels
+
+def parse_example_proto(examples_serialized):
+    feature_map={
+        'image/encoded': tf.FixedLenFeature([], dtype=tf.string,
+                                            default_value=''),
+        'image/class/label': tf.FixedLenFeature([1], dtype=tf.int64,
+                                                default_value=-1),
+        'image/class/text': tf.FixedLenFeature([], dtype=tf.string,
+                                               default_value='')
+        }
+    features=tf.parse_single_example(examples_serialized, feature_map)
+    label = tf.cast(features['image/class/label'], dtype=tf.int32)
+    return features['image/encoded'], label
+
+def distort_color(image, thread_id):
+    color_ordering = thread_id % 2
+    if color_ordering == 0:
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+    elif color_ordering == 1:
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+    image = tf.clip_by_value(image, 0.0, 1.0)
+    return image
+
+def distort_image(image, height, width, thread_id):
+    begin = [399,399]-np.random.randint(10, 2)
+    begin = np.append(begin, 0)
+    size = [389,389,3]
+    distorted_image = tf.slice(image, begin, size)
+    distorted_image = tf.image.random_flip_left_right(distorted_image)
+    distorted_image = distort_color(distorted_image, thread_id)
+    return distorted_image
+
+def image_preprocessing(image_buffer, train, thread_id=0):
+    image = tf.image.decode_jpeg(image_buffer, channels=3)
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    if train:
+        image = distort_image(image, height, width, thread_id)
+    else:
+        image = eval_image(image, height, width)
+    image = tf.subtract(image, 0.5)
+    image = tf.multiply(image, 2.0)
+    return image
+
+#num_readers issue
+def batch_inputs(data_files, batch_size, train, num_preprocess_threads=None,
+                 num_readers=1):
+    if train:
+        filename_queue = tf.train.string_input_producer(data_files,
+                                                        shuffle=True,
+                                                        capacity=16)
+        filename_queue = tf.train.string_input_producer(data_files,
+                                                        shuffle=False,
+                                                        capacity=1)
+    
+    examples_per_shard = 1024
+    #input_queue_memory_factor = 16 works well (inception people)
+    min_queue_examples = examples_per_shard * 16
+    #num_readers issue
+    if train:
+        examples_queue = tf.RandomShuffleQueue(
+            capacity=min_queue_examples+3*batch_size,
+            min_after_dequeue=min_queue_examples,
+            dtypes=[tf.string])
+    else:
+        examples_queue = tf.FIFOQueue(
+            capacity=examples_per_shard + 3 * batch_size,
+            dtypes=[tf.string])
+    reader = tf.TFRecordReader()
+    _, examples_serialized = reader.read(filename_queue)
+    
+    #check how this works
+    images_and_labels=[]
+    for thread_id in range(num_preprocess_threads):
+        image_buffer, label_index = parse_example_proto(examples_serialized)
+        image = image_preprocessing(image_buffer, train, thread_id)
+        images_and_labels.append([image, label_index])
+    images, label_index_batch = tf.train.batch_join(
+        images_and_labels,
+        batch_size=batch_size,
+        capacity=2*num_preprocess_threads*batch_size)
+    
+    height = FLAGS.image_size
+    width = FLAGS.image_size
+    depth = 3
+    
+    images = tf.cast(images, tf.float32)
+    images = tf.reshape(images, shape=[batch_size, height, width, depth])
+    
+    return images, tf.reshape(label_index_batch, [batch_size])
+
+
