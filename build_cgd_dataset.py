2387619d92c0a7ed60b22552ef81ab6df888119e
Tomi Nikolla <tomi.nikolla@gmail.com>
2017-10-26 10:51:11 +0200

    commenting

--- build_cgd_dataset.py
+++ build_cgd_dataset.py
@@ -1,6 +1,13 @@
 #!/usr/local/bin/python
+'''Converts Cornell Grasping Dataset data into TFRecords data format using Example protos.
+The raw data set resides in png and txt files located in the following structure:
+    
+    dataset/03/pcd0302r.png
+    dataset/03/pcd0302cpos.txt
+'''
+
+
 import tensorflow as tf
-from itertools import islice
 import os
 import glob
 import numpy as np
@@ -9,8 +16,6 @@ dataset = '/root/dataset/cornell_grasping_dataset'
 #49
 #dataset = '/home/tomi/py/dataset'
 #32
-# If the directory of the current script lies where the out_dir will be
-#output_directory = os.path.dirname(os.path.abspath(__file__))
 #train_filename = 'train-cgd'
 #validation_filename = 'validation-cgd'
 
@@ -24,16 +29,20 @@ class ImageCoder(object):
                               feed_dict={self._decode_png_data: image_data})
 
 def _process_image(filename, coder):
+    # Decode the image
     with open(filename) as f:
         image_data = f.read()
     image = coder.decode_png(image_data)
     assert len(image.shape) == 3
     height = image.shape[0]
     width = image.shape[1]
-    assert image.shape[2] == 3
+    assert image.shape[2] == 3    
     return image_data, height, width
 
+
 def _process_bboxes(name):
+    '''Create a list with the coordinates of the grasping rectangles. Every 
+    element is either x or y of a vertex.'''
     with open(name, 'r') as f:
         bboxes = list(map(
               lambda coordinate: float(coordinate), f.read().strip().split()))
@@ -53,6 +62,7 @@ def _bytes_feature(v):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[v]))
 
 def _convert_to_example(filename, bboxes, image_buffer, height, width):
+    # Build an Example proto for an example
     example = tf.train.Example(features=tf.train.Features(feature={
           'image/filename': _bytes_feature(filename),
           'image/encoded': _bytes_feature(image_buffer),
@@ -69,34 +79,39 @@ def main():
     print(validation_file)
     writer_train = tf.python_io.TFRecordWriter(train_file)
     writer_validation = tf.python_io.TFRecordWriter(validation_file)
-    
+   
+    # Creating a list with all the image paths
     folders = range(1,11)
     folders = ['0'+str(i) if i<10 else '10' for i in folders]
     filenames = []
-    
     for i in folders:
         for name in glob.glob(os.path.join(dataset, i, 'pcd'+i+'*r.png')):
             filenames.append(name)
     
+    # Shuffle the list of image paths
     np.random.shuffle(filenames)
     
     count = 0
-    a=0
-    b=0
+    valid_img = 0
+    train_img = 0
+
     coder = ImageCoder()
     for filename in filenames:
         bbox = filename[:49]+'cpos.txt'
         bboxes = _process_bboxes(bbox)
         image_buffer, height, width = _process_image(filename, coder)
         example = _convert_to_example(filename, bboxes, image_buffer, height, width)
+        # Split the dataset in 80% for training and 20% for validation
         if count % 5 == 0:
             writer_validation.write(example.SerializeToString())
-            a+=1
+            valid_img +=1
         else:
             writer_train.write(example.SerializeToString())
-            b+=1
-        count = count + 1
-    print(a, b)
+            train_img +=1
+        count +=1
+    
+    print('Done converting %d images in TFRecords with %d train images and %d validation images' % (count, train_img, valid_img)))
+    
     writer_train.close()
     writer_validation.close()
 
